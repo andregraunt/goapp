ec4e5e40c349f8b1b8f858bd9f7f1a4191c28d85
pifpafoyoyoy <smitsmit101@gmail.com>
2022-12-02 00:38:35 +0200

    First

---
+++ main.go
@@ -0,0 +1,30 @@
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	name := "vasa"
+// 	age := 37
+// 	maskoret := 5000.55
+// 	fmt.Println("hello go")
+// 	fmt.Printf("My name is %s, My age %d and I gonna %f dollars ", name, age, maskoret)
+// }
+
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	const col = 30
+	// Clear the screen by printing \x0c.
+	bar := fmt.Sprintf("\x0c[%%-%vs]", col)
+	for i := 0; i < col; i++ {
+		fmt.Printf(bar, strings.Repeat("=", i)+">")
+		time.Sleep(500 * time.Millisecond)
+	}
+	fmt.Printf(bar+" Done!", strings.Repeat("=", col))
+}
